ce7c0776bdc0de5d7106a1d5755ef828509c83ba
he1fire <cmg3172@naver.com>
2024-05-26 10:54:33 +0900

    예제 입출력 추가

--- C++/BinarySearch.cpp
+++ C++/BinarySearch.cpp
@@ -4,29 +4,16 @@ using namespace std;
 ll N, M;
 ll arr[1005];
 bool BinarySearch(ll x){
-    ll lo=0, hi=N-1;
-    while (lo<=hi){
+    ll lo=0, hi=N;
+    while (lo+1<hi){
         ll mid=(lo+hi)/2;
-        if (arr[mid]<x)
-            lo=mid+1;
-        else if (arr[mid]>x)
-            hi=mid-1;
+        if (arr[mid]<=x)
+            lo=mid;
         else
-            return 1;
+            hi=mid;
     }
-    return 0;
+    return arr[lo]==x;
 }
-// bool BinarySearch(ll x){
-//     ll lo=0, hi=N;
-//     while (lo+1<hi){
-//         ll mid=(lo+hi)/2;
-//         if (arr[mid]<=x)
-//             lo=mid;
-//         else
-//             hi=mid;
-//     }
-//     return arr[lo]==x;
-// }
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
--- Python/LineIntersection.py
+++ Python/LineIntersection.py
@@ -1,5 +1,3 @@
-import sys
-
 class Dot:
     def __init__(self, x, y):
         self.x=x
